dd7adf13a5146a42dcaf20f60b091fc0948ec1da
krisescobedo <kristianescobedo@gmail.com>
2018-07-02 16:16:38 -0300

    Hechos ejercicios 1, 2 y 3
    Lo de la derecha asigna lo que esta al izquierdo

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,13 @@
 function mostrar()
 {
+	var precio;
+	var descuento;
+	var precioFinal;
+	
+	precio = parseInt(prompt("ingresar el precio"));
+	descuento = parseInt(prompt("agregar el descuento(en porcentaje)"));
+	precioFinal = (precio - (precio*descuento/100));
+
+	document.getElementById('elPrecioFinal').value = precioFinal
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,12 @@
 
 function mostrar()
 {
+	var largo;
+	var ancho;
+	var perimetro;
 
+	largo = 2*parseInt(prompt("ingresar largo"));
+	ancho = parseInt(prompt("ingresar ancho"));
+	perimetro = (largo + ancho);
+	alert("el perimetro es: " + perimetro);
 }
