5675337e697579b3dbe9f9515547adb9920ded46
krisescobedo <kristianescobedo@gmail.com>
2018-07-04 12:17:10 -0300

    Base * 3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,25 @@
 function mostrar()
 {
+	var precio;
+	var porcentaje;
+	var descuento;
+	var precioConDescuento;
+	var iva;
+	var precioFinal;
+	precio = prompt("ingrese el precio");
+	precio = parseInt(precio);
+	porcentaje = prompt("ingrese el porcentaje de descuento");
+	porcentaje = parseInt(porcentaje);
+	descuento = precio * porcentaje / 100;
+	precioConDescuento = precio - descuento;
+	iva = precioConDescuento*21/100;
+	precioFinal = precioConDescuento + iva;
+
+	document.getElementById('elPrecioFinal').value = precioFinal
+
+	alert("El descuento es de "+descuento+" el precio final es de "+precioConDescuento+ " y el impuesto de valor agregado es de "+iva);
+
+
+
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,11 @@
 
 function mostrar()
 {
-
+	var base;
+	var altura;
+	var perimetro;
+	base = prompt("ingrese la base");
+	altura = prompt("ingrese la altura");
+	perimetro = base *  3;
+	alert("el perimetro del triangulo es " + perimetro);
 }
