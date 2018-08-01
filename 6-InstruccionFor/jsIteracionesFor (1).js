46d99cfe44aebb7a8dc8e0087da0b47672cdce85
krisescobedo <kristianescobedo@gmail.com>
2018-08-01 12:17:02 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,13 @@
 function mostrar()
 {
+/* While: cuando no conozco de antemando la cantidad de interacciones
+	For: Cunad si conozco la cantida de interacciones
+	Do While: Cuando el ciclo se ejecuta por lo menos una vez
+*/
+
+for (var contador=1; contador<=10; contador++){
+	alert(contador);
+	}
+
 
 }
--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,14 @@
 function mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
+var repeticiones = parseInt(prompt("ingrese el número de repeticiones"));
+var i;
 
+// En for se tienen que dictar 3 sentencias para que sae correcta
+for (  i=0 ; i<repeticiones; i++)
+{
+	console.log("Hola UTN FRA");
+}
 
 
 }//FIN DE LA FUNCIÓN
